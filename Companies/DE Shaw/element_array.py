f3248947330118d96364a372b871503184dd8d55
Manmeet Tarun <meetutarun16@gmail.com>
2018-08-04 15:59:11 +0530

    DE Shaw

---
+++ Companies/DE Shaw/element_array.py
@@ -0,0 +1,13 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    l=list(map(int,input().split()))
+    f=0
+    for j in range(0,n,2):
+	if(j!=n-1):
+	    print ("J val; ",j)
+            if(l[j]!=l[j+1]):
+                f=1
+                print (l[j])
+        elif j==n-1 and f==0:
+            print (l[j])
