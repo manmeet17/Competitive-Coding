b68dcd4ee5a389073dd42f2bdc6c01bab6b103a4
Manmeet Tarun <meetutarun16@gmail.com>
2018-09-30 15:18:10 +0530

    Done

---
+++ Companies/Microsoft/matrix_paths.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n,m=map(int,input().split())
+    dp=[[0 for i in range(m)] for j in range(n)]
+    for i in range(m):
+        dp[0][i]=1
+    for i in range(n):
+        dp[i][0]=1
+    for i in range(1,n):
+        for j in range(1,m):
+            dp[i][j]=dp[i-1][j]+dp[i][j-1]
+    print (dp[-1][-1]%1000000007)
+    
